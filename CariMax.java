9125614a7b0e8ce89110b34df67e0ff02c59095b
Gugi Gustaman <hyourinseta@live.com>
2017-01-28 13:50:27 +0700

    Melengkapi method main

--- CariMax.java
+++ CariMax.java
@@ -1,5 +1,15 @@
 class CariMax {
 	public static void main (String[] args) {
+		int angka1 = 60;
+		int angka2 = 30;
+		int angka3 = 70;
+
+		int max = 0;
+
+		max = angka1 >= max ? angka1 : max;
+		max = angka2 >= max ? angka2 : max;
+		max = angka3 >= max ? angka3 : max;
 		
+		System.out.println("Angka terbesar adalah "+max);
 	}
 }
